435b28a00b8748f8929587d8e1e275731c44aa90
Aaron-24DucAnh24 <90702045+Aaron-24DucAnh24@users.noreply.github.com>
2022-09-18 23:05:40 +0700

    Duc Anh

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 
+// slide handling
+
+const slides = document.querySelectorAll('.slide')
+var i = 1
+setInterval(function() {
+    slides.forEach(function(slide){
+        slide.style = 'display: none;'
+    })
+    slides[i].style = 'display: block;'
+    i++
+    if(i > 2) i = 0
+}, 5000);
+
+// password witching
+
+var passwordInput = document.querySelector(".authen__password-input")
+function passwordSwitch() {
+    if(passwordInput.type === 'password') {
+        passwordInput.type = 'text'
+    } else {
+        passwordInput.type = 'password'
+    }
+}
