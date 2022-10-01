d402b42201fb748077188a87bef2b21ba4e46dcb
Aaron-24DucAnh24 <90702045+Aaron-24DucAnh24@users.noreply.github.com>
2022-10-01 10:19:55 +0700

    Duc Anh

--- js/main.js
+++ js/main.js
@@ -14,29 +14,21 @@ setInterval(function() {
 // password witching----------------------------
 
 var passwordInput = document.querySelector(".authen__password")
+var passwordIcon = document.querySelector('.password-icon')
 function passwordSwitch() {
     if(passwordInput.type === 'password') {
         passwordInput.type = 'text'
     } else {
         passwordInput.type = 'password'
     }
-}
 
-// authentication handling
-
-var submitBtn = document.querySelector('.authen__btn')
-var loginNameInput = document.querySelector('.authen__login-name')
-var loginNameValue
-var passwordValue
-
-loginNameInput.oninput = function(e) {
-    loginNameValue = e.target.value
-}
-
-passwordInput.oninput = function(e) {
-    passwordValue = e.target.value
-}
-
-submitBtn.onclick = function(e) {
+    if (passwordIcon.classList.contains('fa-eye-slash')) {
+        passwordIcon.classList.remove('fa-eye-slash')
+        passwordIcon.classList.add('fa-eye')
+    } 
     
+    else {
+        passwordIcon.classList.remove('fa-eye')
+        passwordIcon.classList.add('fa-eye-slash')
+    }
 }
