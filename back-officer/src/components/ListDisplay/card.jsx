d7e348f406427c7ff1feaa4c755572057bd36eaf
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2022-11-24 08:32:23 +0700

    Change notification icon
    Chưa có css của cái icon

--- back-officer/src/components/ListDisplay/card.jsx
+++ back-officer/src/components/ListDisplay/card.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import "./ListDisplay.css";
 import infoIcon from "./img/infoIcon.png";
+import * as ImIcons from "react-icons/im";
 
-export const Card = ({cardProp}) => {
-
+export const Card = ({ cardProp }) => {
     // TODO Tuan
-    const ShowDetail = () =>{
+    const ShowDetail = () => {
         console.log("DetailInfo handle " + cardProp.name);
-    }
+    };
 
-    return (<button className="cardBox" onClick={ShowDetail}>
-        <div className="cardName">
-            {cardProp.name}
-        </div>
+    return (
+        <button className="cardBox" onClick={ShowDetail}>
+            <div className="cardName">{cardProp.name}</div>
 
-        <div className="cardStatus">
-            {cardProp.status == true ? <span id="online">Online</span> : "Offline"}
-        </div>
-
-        <img src={infoIcon} className="icon"></img>
-
-
-    </button>);
+            <div className="cardStatus">
+                {cardProp.status === true ? (
+                    <span id="online">Online</span>
+                ) : (
+                    "Offline"
+                )}
+            </div>
+            {/*Looking not so good, lol */}
+            <ImIcons.ImNotification />
+            {/*<img src={infoIcon} className="icon"></img>*/}
+        </button>
+    );
 };
-
