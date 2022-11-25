ea019dfa3694c8023f889bd779c5ea21d83390f3
WindyFeg <phongspeed7@yahoo.com.vn>
2022-11-25 20:44:20 +0700

    minor data change

--- back-officer/src/components/ListDisplay/card.jsx
+++ back-officer/src/components/ListDisplay/card.jsx
@@ -34,7 +34,7 @@ export const Card = ({ cardProp, objectCard }) => {
             <button className="cardBox" onClick={ShowDetail}>
                 <div className="cardName">{cardProp.name}</div>
                 <div className="cardStatus">
-                    {typeof (cardProp.isUsed) == 'boolean' ? (IsUsed) : (typeof (cardProp.status) == 'boolean' ? (ChangeStatus) : cardProp.status)}
+                    {typeof (cardProp.isUsed) == 'boolean' ? (IsUsed) : (typeof (cardProp.status) == 'boolean' ? (ChangeStatus) : cardProp.status + "%")}
                 </div>
                 <infoIcons.RiInformationLine id="iconInfo" />
             </button>
