17d670520ff23d1db5fc753474d13f6641362ec4
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2022-11-25 11:50:28 +0700

    Add detail box of vehicle
    have bugs

--- back-officer/src/components/DetailInfo/CollectorInfo.js
+++ back-officer/src/components/DetailInfo/CollectorInfo.js
@@ -1,7 +1,64 @@
 import React from "react";
+import "./DetailInfo.css";
 
-function CollectorInfo() {
-    return <div>CollectorInfo</div>;
+function CollectorInfo(props) {
+    // employeeList={props.employee} employee={props.name}
+    return (
+        <div>
+            {props.employeeList
+                .filter((emp) => emp.name === props.employee)
+                .map((filteredEmp) => {
+                    return (
+                        <div key={filteredEmp.name}>
+                            <ul className="detailInformation-name__ul">
+                                <li>
+                                    <b>Mã nhân viên: </b>
+                                    {filteredEmp.employeeID}
+                                </li>
+                                <li>
+                                    <b>Tên: </b>
+                                    {filteredEmp.name}
+                                </li>
+                                <li>
+                                    <b>Chức vụ: </b>
+                                    {filteredEmp.chucVu}
+                                </li>
+                                <li>
+                                    <b>Trạng thái: </b>
+                                    {filteredEmp.status}
+                                </li>
+                            </ul>
+                            <ul className="detailInformation-name__ul">
+                                <li>
+                                    <b>Phương tiện: </b>
+                                    <span className="detailInformation-name__wrap">
+                                        {filteredEmp.vehicle}
+                                    </span>
+                                </li>
+                            </ul>
+                            <ul className="detailInformation-name__ul">
+                                <li>
+                                    <b>Tuyến đường:</b>
+                                    <ul>
+                                        {filteredEmp.route.map((_route) => {
+                                            return (
+                                                <li
+                                                    key={_route.name}
+                                                    className="detailInformation-name__wrap"
+                                                >
+                                                    <b>{_route.name} </b>{" "}
+                                                    {_route.address}
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    );
+                })}
+        </div>
+    );
 }
 
 export default CollectorInfo;
--- back-officer/src/components/DetailInfo/VehicleInfo.js
+++ back-officer/src/components/DetailInfo/VehicleInfo.js
@@ -1,7 +1,51 @@
 import React from "react";
 
-function VehicleInfo() {
-    return <div>VehicleInfo</div>;
+function VehicleInfo(props) {
+    return (
+        <div>
+            {props.vehicleList
+                .filter((veh) => veh.name === props.vehicle)
+                .map((filteredVeh) => {
+                    return (
+                        <div key={filteredVeh.name}>
+                            <ul className="detailInformation-name__ul">
+                                <li>
+                                    <b>Tên phương tiện: </b>
+                                    {filteredVeh.name}
+                                </li>
+                                <li>
+                                    <b>Biển số xe: </b>
+                                    {filteredVeh.code}
+                                </li>
+                                <li>
+                                    <b>Trọng tải: </b>
+                                    {filteredVeh.weight}
+                                </li>
+                            </ul>
+                            <ul className="detailInformation-name__ul">
+                                <li>
+                                    <b>Người sử dụng hiện tại: </b>
+                                    <br></br>
+                                    <span className="detailInformation-name__wrap">
+                                        {filteredVeh.empUsing}
+                                    </span>
+                                </li>
+                            </ul>
+                            <ul className="detailInformation-name__ul">
+                                <li>
+                                    <b>Vị trí hiện tại:</b>
+                                    <span className="detailInformatio-name__wrap">
+                                        <b>{filteredVeh.locationName} </b>{" "}
+                                        {filteredVeh.address}
+                                        {console.log(filteredVeh.locationName)}
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    );
+                })}
+        </div>
+    );
 }
 
 export default VehicleInfo;
