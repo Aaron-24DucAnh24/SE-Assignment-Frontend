dca152cf94486407a881f095f14ea5f95c972d07
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2022-11-27 16:40:06 +0700

    fix calendar

--- collector/src/App.js
+++ collector/src/App.js
@@ -1,33 +1,36 @@
-import logo from './logo.svg';
 import React from "react";
-import './App.css';
+import "./App.css";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import WeekCalendar from 'react-week-calendar';
-import 'react-week-calendar/dist/style.less';
+import "react-week-calendar/dist/style.less";
 
 // import "@fullcalendar/core/main.css";
 // import "@fullcalendar/daygrid/main.css";
 // import "@fullcalendar/timegrid/main.css";
 
 function App() {
-  const events = [{ title: "today's event", date: new Date() }];
-  return (
-    <div className="App">
-      {/* <FullCalendar
-        defaultView="timeGridWeek"
-        header={{
-          left: "prev,next",
-          center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay"
-        }}
-        plugins={[dayGridPlugin, timeGridPlugin]}
-        events={events}
-      /> */}
-      <WeekCalendar/>
-    </div>
-  );
+    const events = [
+        { title: "today's event", date: new Date() },
+        { title: "lol", date: new Date("2022-11-28") },
+        { title: "lol", date: new Date("2022-11-29") },
+        { title: "lol", date: new Date("2022-11-29:16:59:00") },
+    ];
+    return (
+        <div className="calendar">
+            <FullCalendar
+                defaultView="timeGridWeek"
+                header={{
+                    left: "prev,next",
+                    center: "title",
+                    right: "dayGridMonth,timeGridWeek,timeGridDay",
+                }}
+                plugins={[dayGridPlugin, timeGridPlugin]}
+                events={events}
+            />
+            <button>checkin</button>
+        </div>
+    );
 }
 
 export default App;
