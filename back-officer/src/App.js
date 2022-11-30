67a8b555a8eb2a4686362c344c32bc7009cfdc72
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2022-11-30 16:42:18 +0700

    Thiết lập API back-officer

--- back-officer/src/App.js
+++ back-officer/src/App.js
@@ -2,14 +2,28 @@ import Header from "./components/Header/Header.js";
 import Confirm from "./components/Confirm/btnConfirm";
 import ListDisplay from "./components/ListDisplay/ListDisplay.js";
 import Footer from "./components/footer/footer.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import EmpDumb from "./components/Data/EmpDumb.json";
 import McpDumb from "./components/Data/McpDumb.json";
 import VehicleDumb from "./components/Data/VehicleDumb.json";
 import Datepicker from "./components/DatePicker/Datepicker.js";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+    const [Collector, setCollector] = useState([]);
+
+    useEffect(() => {
+        getCollectors();
+    }, []);
+
+    const getCollectors = async () => {
+        const response = await axios.get(
+            "https://localhost:7271/api/Collector"
+        );
+        setCollector(response.data);
+    };
+
     const [Employee, SetEmployee] = useState(EmpDumb);
 
     const [Vehicle, SetVehicle] = useState(VehicleDumb);
@@ -19,6 +33,7 @@ function App() {
     return (
         <div>
             <Header />
+            {console.log(Collector[0].name + Collector[0].eId)}
             <div className="Body">
                 <div className="calendar">
                     Calendar
