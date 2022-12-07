f1c1252b3a16e6a45d7cf427dd33843d29f9abce
WindyFeg <phongspeed7@yahoo.com.vn>
2022-12-07 14:25:25 +0700

    can't do login fuction

--- back-officer/src/login/Login.js
+++ back-officer/src/login/Login.js
@@ -5,8 +5,30 @@ import img3 from "./img/slide2.webp"
 import Header from "./Header/Header.js";
 import Footer from "./footer/footer.js";
 import "./slides"
+import { useState, useEffect } from "react";
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const UsernameHandler = event => {
+        setUsername(event.target.value);
+        console.log('value is:', event.target.value);
+        console.log(username);
+    }
+
+    const PasswordHandler = event => {
+        setPassword(event.target.value);
+        console.log('value is:', event.target.value);
+        console.log(password);
+    }
+
+    const LoginHandler = () => {
+        if (username === password && username == "backofficer") {
+            console.log("login as backofficer")
+        }
+    }
+
     // password witching----------------------------
 
     const passwordSwitch = () => {
@@ -31,25 +53,52 @@ const Login = () => {
 
     return (
         <div>
-            <Header/>
+            <Header />
             <div className="body">
                 <div className="authen-section">
                     <div className="authen__wrap">
-                        <div className="authen__label">Đăng nhập vào hệ thống</div>
-                        <div className="authen__desc">Sử dụng <i>Tên đăng nhập</i> và <i>Mật khẩu</i> được cung cấp</div>
+                        <div className="authen__label">
+                            Đăng nhập vào hệ thống
+                        </div>
+                        <div className="authen__desc">
+                            Sử dụng
+                            <i>
+                                Tên đăng nhập
+                            </i>
+                            và
+                            <i>
+                                Mật khẩu
+                            </i>
+                            được cung cấp
+                        </div>
                         <form action="/back-officer" className="authen__form">
                             <div className="authen__input-wrap">
-                                <input name="loginName" maxLength="124" type="text" className="authen__input authen__login-name"
-                                    placeholder="Tên đăng nhập" />
+                                <input
+                                    name="loginName"
+                                    maxLength="124"
+                                    type="text"
+                                    className="authen__input authen__login-name"
+                                    placeholder="Tên đăng nhập"
+                                    value={username}
+                                    onChange={UsernameHandler}
+                                />
                             </div>
-                            <div className="authen__input-wrap authen__input-wrap--password">
-                                <input name="passWord" type="password" maxLength="16" className="authen__input authen__password"
-                                    placeholder="Mật khẩu" />
+                            <div
+                                className="authen__input-wrap authen__input-wrap--password">
+                                <input
+                                    name="passWord"
+                                    type="password"
+                                    maxLength="16"
+                                    className="authen__input authen__password"
+                                    placeholder="Mật khẩu"
+                                    value={password}
+                                    onChange={PasswordHandler}
+                                />
                                 <label className="authen__input-icon clickable" htmlFor="password-swicher"><i
                                     className="fa-solid fa-eye-slash password-icon"></i></label>
                                 <input type="checkbox" id="password-swicher" style={{ display: "none" }} onClick={passwordSwitch} />
                             </div>
-                            <button className="authen__btn clickable" >
+                            <button className="authen__btn clickable" onClick={LoginHandler}>
                                 <a href="/back-officer" >
                                     Đăng nhập
                                 </a>
@@ -63,7 +112,7 @@ const Login = () => {
                     <img src={img3} alt="" className="slide" />
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 };
